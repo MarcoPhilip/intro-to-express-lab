dfe2e49315080b97368d2b4ea3f6980aaff6cf46
marcophilip <marcophilipagapito@gmail.com>
2025-03-19 19:12:26 -0700

    lab is finished

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const collectibles = [
     { name: 'shiny ball', price: 5.95 },
     { name: 'autographed picture of a dog', price: 10 },
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
-  ];
+];
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
 
 
 console.log("I'm Starting");
@@ -72,13 +82,77 @@ Copy
 Validation: If the index does not correspond to an item in the array, respond with “This item is not yet in stock. Check back soon!”
 
 Response: Should describe the item at the given index, like “So, you want the shiny ball? For 5.95, it can be yours!” Include both the name and price properties.
-
-
 */
 
+
 app.get('/collectibles/:indexParam', (req, res) => {
-    res.send(`So, you want the ${collectibles[req.params.indexParam].name}? For ${collectibles[req.params.indexParam].price}, it can be yours!`)
+    // parse input index
+    const itemIdx = parseInt(req.params.indexParam);
+    //if the route input is a number, less than the length of the collectibles but not less than 0, send the item
+    if (!isNaN(itemIdx) && itemIdx >= 0 && itemIdx < collectibles.length) {
+        //create const for the the name 
+        const itemName = collectibles[req.params.indexParam].name;
+        //create const for the price
+        const itemPrice = collectibles[req.params.indexParam].price;
+        //return the item
+        res.send(`So, you want the ${itemName}? For ${itemPrice}, it can be yours!`);
+    }
+    //if not, throw an error
+    else {
+        res.status(400).send("This item is not yet in stock. Check back soon!")
+    }
 })
+
+
+/*
+4. Filter Shoes by Query Parameters
+Use the following array of shoes in this challenge:
+
+Copy
+  const shoes = [
+      { name: "Birkenstocks", price: 50, type: "sandal" },
+      { name: "Air Jordans", price: 500, type: "sneaker" },
+      { name: "Air Mahomeses", price: 501, type: "sneaker" },
+      { name: "Utility Boots", price: 20, type: "boot" },
+      { name: "Velcro Sandals", price: 15, type: "sandal" },
+      { name: "Jet Boots", price: 1000, type: "boot" },
+      { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+  ];
+Task: Create a route /shoes that filters the list of shoes based on query parameters.
+
+Query Parameters:
+
+min-price: Excludes shoes below this price.
+max-price: Excludes shoes above this price.
+type: Shows only shoes of the specified type.
+No parameters: Responds with the full list of shoes.
+*/
+
+app.get('/shoes', (req, res) => {
+    //create the variables for the query params
+    let {minPrice, maxPrice, type} = req.query;
+
+    let shoeLists = shoes;
+
+    if (minPrice) {
+        //if min price, filter the shoes = or > to the input
+        shoeLists = shoeLists.filter(shoe => shoe.price >= parseFloat(minPrice));
+    }
+    
+    if (maxPrice) {
+        //if max price, filter the shoes = or < to the input
+        shoeLists = shoeLists.filter(shoe => shoe.price <= parseFloat(maxPrice));
+    }
+    
+    if (type) {
+        //if input is type, filter out the shoe types with only the same input 
+        shoeLists = shoeLists.filter(shoe => shoe.type.toLowerCase() === type.toLowerCase());
+    }
+    //if no parameter(/shoes), show the full list of shoes
+    res.send(shoeLists);
+});
+
+
 
 //listen for the request on port 3000
 app.listen(3000, () => {
